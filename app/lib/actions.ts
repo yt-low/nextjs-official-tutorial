68ebe9bda451553988a388e4ece219e6c1cd3978
Yeow Thiong Low <yeowthionglow@MacBook-Air.local>
2026-01-20 15:50:17 +0800

    Add NextAuth to login page.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { signIn } from '../auth';
+import { AuthError } from 'next-auth';
 
 const sql = postgres(process.env.DATABASE_URL!, { ssl: 'require' });
 
@@ -102,4 +104,23 @@ export async function deleteInvoice(id: string) {
     throw new Error("Errrrrr")
     await sql `DELETE FROM invoices WHERE id = ${id}`
     revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
